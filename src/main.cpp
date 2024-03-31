aa8b549967a3aa664f0ca05c4746e5293c55d86d
Nikita Koschin <nik.koshin@mail.ru>
2024-03-31 16:19:52 +0300

    task execution refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -2,60 +2,53 @@
 #include "bmp.h"
 #include "BMPProcess.h"
 
+void task3(const std::string& imagePath);
+void task4a(const std::string& imagePath);
+void task4b(const std::string& imagePath);
+
 int main(int argc, char* argv[])
 {
 //    Default program run
-    if (argc < 2)
-    {
+    if (argc < 2) {
         return 0;
     }
 
-    if (strcmp(argv[1], "-ch") == 0)
-    {
-        std::string imagePath = argv[2];
-        std::string redPath = imagePath.substr(0, imagePath.size() - 4) + ".red.bmp";
-        std::string greenPath = imagePath.substr(0, imagePath.size() - 4) + ".green.bmp";
-        std::string bluePath = imagePath.substr(0, imagePath.size() - 4) + ".blue.bmp";
-
-        BMP bmp_image = load_bmp_image(imagePath);
-        BMPProcess bmp_process = BMPProcess(bmp_image);
-
-        save_bmp_image(bmp_process.get_blue_channel(), bluePath);
-        save_bmp_image(bmp_process.get_red_channel(), redPath);
-        save_bmp_image(bmp_process.get_green_channel(), greenPath);
-
-        std::cout << redPath << std::endl;
-        std::cout << greenPath << std::endl;
-        std::cout << bluePath << std::endl;
-
-        return 0;
+    if (strcmp(argv[1], "-task3") == 0){
+        task3(argv[2]);
+    } else if (strcmp(argv[1], "-task4a") == 0) {
+        task4a(argv[2]);
+    } else if (strcmp(argv[1], "-task4b") == 0) {
+        task4b(argv[2]);
     }
-    if (strcmp(argv[1], "-correlation") == 0)
-    {
-        std::string imagePath = argv[2];
+}
 
-        BMP bmp_image = load_bmp_image(imagePath);
-        BMPProcess bmp_process = BMPProcess(bmp_image);
-        std::cout << "{" << std::endl <<
-                "\t\"R&B\": " << bmp_process.correlation('r', 'b') << "," << std::endl <<
-                "\t\"R&G\": " << bmp_process.correlation('r', 'g') << "," << std::endl <<
-                "\t\"G&B\": " << bmp_process.correlation('g', 'b') << std::endl <<
-                "}" << std::endl;
+void task3(const std::string& imagePath) {
+    std::string redPath = imagePath.substr(0, imagePath.size() - 4) + ".red.bmp";
+    std::string greenPath = imagePath.substr(0, imagePath.size() - 4) + ".green.bmp";
+    std::string bluePath = imagePath.substr(0, imagePath.size() - 4) + ".blue.bmp";
 
-        return 0;
-    }
-    if (strcmp(argv[1], "-autocorrelation") == 0)
-    {
-        std::string imagePath = argv[2];
+    BMP bmp_image = load_bmp_image(imagePath);
+    BMPProcess bmp_process = BMPProcess(bmp_image);
 
-        BMP bmp_image = load_bmp_image(imagePath);
-        BMPProcess bmp_process = BMPProcess(bmp_image);
-        std::cout << "{" << std::endl <<
-                  "\t\"R&B\": " << bmp_process.correlation('r', 'b') << "," << std::endl <<
-                  "\t\"R&G\": " << bmp_process.correlation('r', 'g') << "," << std::endl <<
-                  "\t\"G&B\": " << bmp_process.correlation('g', 'b') << std::endl <<
-                  "}" << std::endl;
+    save_bmp_image(bmp_process.get_blue_channel(), bluePath);
+    save_bmp_image(bmp_process.get_red_channel(), redPath);
+    save_bmp_image(bmp_process.get_green_channel(), greenPath);
 
-        return 0;
-    }
+    std::cout << redPath << std::endl;
+    std::cout << greenPath << std::endl;
+    std::cout << bluePath << std::endl;
+}
+
+void task4a(const std::string& imagePath) {
+    BMP bmp_image = load_bmp_image(imagePath);
+    BMPProcess bmp_process = BMPProcess(bmp_image);
+    std::cout << "{" << std::endl <<
+              "\t\"R&B\": " << bmp_process.correlation('r', 'b') << "," << std::endl <<
+              "\t\"R&G\": " << bmp_process.correlation('r', 'g') << "," << std::endl <<
+              "\t\"G&B\": " << bmp_process.correlation('g', 'b') << std::endl <<
+              "}" << std::endl;
+}
+
+void task4b(const std::string& imagePath) {
+
 }
